a4e908b84b381b331899d940260854fc87636b39
mouse07410 <uri@mit.edu>
2023-02-03 00:19:41 -0500

    Aded -t epub3 option

--- src/convert_book/options.rs
+++ src/convert_book/options.rs
@@ -4,6 +4,6 @@ pub const MARKDOWN: &'static str = "markdown+smart+grid_tables+pipe_tables-simpl
 
 pub const HTML:  &'static str = "--embed-resources --standalone --highlight-style=tango --table-of-contents --section-divs --template=lib/template.html --css=lib/pandoc.css --to=html5";
 
-pub const EPUB:  &'static str = "--embed-resources --standalone --highlight-style=tango --css=lib/epub.css --table-of-contents";
+pub const EPUB:  &'static str = "--embed-resources --standalone --highlight-style=tango --css=lib/epub.css -t epub3 --table-of-contents";
 
 pub const LATEX: &'static str = "--embed-resources --standalone --highlight-style=tango --top-level-division=chapter --table-of-contents --template=lib/template.tex --pdf-engine=xelatex --to=latex --pdf-engine-opt=--interaction=nonstopmode";
